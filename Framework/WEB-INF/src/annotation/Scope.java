4a6d86eeef89c6de442d71949a9df8cd59246925
tafita37 <118960434+tafita37@users.noreply.github.com>
2023-11-19 22:42:01 +0300

    Sprint 15
    Effacer sessions

--- Framework/WEB-INF/src/annotation/Scope.java
+++ Framework/WEB-INF/src/annotation/Scope.java
@@ -9,5 +9,5 @@ import java.lang.annotation.Target;
 @Target(ElementType.TYPE)
 
 public @interface Scope {
-    String singleton();
+    boolean singleton() default true;
 }
--- Test-Framework/WEB-INF/src/controllers/Employes.java
+++ Test-Framework/WEB-INF/src/controllers/Employes.java
@@ -6,7 +6,7 @@ import annotation.*;
 import upload.FileUpload;
 import url.ModelView;
 
-@Scope(singleton = "Singleton")
+@Scope(singleton = true)
 public class Employes {
     int id_employes;
     String nom;
@@ -92,48 +92,130 @@ public class Employes {
     }
 
 /*------------------------------------------Fonctions principales------------------------------------------ */
-/// Find all
-    @Url(link = ".do")
-    public ModelView completeInformation()
+/// Sprint 4
+    @Url(link = "sprint-4.do")
+    public ModelView sprint4()
     throws Exception {
-        ModelView result=new ModelView("formulaire.html");
-        // result.setJson(true);
-        result.addItem("testJson", new Employes());
-        result.addItem("testJson2", "new Employes()");
-        return result;
+        ModelView resut=new ModelView("sprint-4.jsp");
+        return resut;
     }
 
-/// Deuxième test
-    @Url(link = "test.do")
-    public ModelView allEmp()
+/// Sprint 5
+    @Url(link = "sprint-5.do")
+    public ModelView sprint5() 
     throws Exception {
-        ModelView result=new ModelView("test.jsp");
-        result.addItem("employe", this);
-        result.addSession("role", "admin");
-        result.addSession("isConnected", true);
-        return result;
+        ModelView resut=new ModelView("sprint-5.jsp");
+        return resut;
     }
 
-/// Autorisation
-    @Auth(role = "admin", exception = "Seule l'admin peut acceder a la fonction admin")
-    @Url(link = "admin.do")
-    public ModelView admin()
+/// Sprint 6
+    @Url(link = "sprint-6.do")
+    public ModelView sprint6()
     throws Exception {
-        ModelView md=new ModelView("admin.jsp");
-        System.out.println(this.getSession().get("role"));
+        ModelView resut=new ModelView("sprint-6.jsp");
+        resut.addItem("emp", this);
+        resut.addItem("emp2", new Employes(1, "nom", "prenom"));
+        return resut;
+    }
+
+/// Sprint 7
+    @Url(link = "sprint-7.do")
+    public ModelView sprint7()
+    throws Exception {
+        ModelView resut=new ModelView("sprint-7.jsp");
+        resut.addItem("emp", this);
+        return resut;
+    }
+
+/// Sprint 8
+    @Url(link = "sprint-8.do")
+    @Parameters(args = {"id_emp"})
+    public ModelView sprint8(int id_emp)
+    throws Exception {
+        ModelView resut=new ModelView("sprint-8.jsp");
+        resut.addItem("id_emp", id_emp);
+        return resut;
+    }
+    
+/// Sprint 9
+    @Url(link = "sprint-9.do")
+    public ModelView sprint9()
+    throws Exception {
+        ModelView resut=new ModelView("index.html");
+        return resut;
+    }
+
+/// Sprint 10 
+    @Url(link = "sprint-10.do")
+    public ModelView sprint10()
+    throws Exception {
+        ModelView resut=new ModelView("index.html");
+        return resut;
+    }
+
+/// login
+    @Url(link = "login.do")
+    public ModelView login()
+    throws Exception {
+        ModelView md=new ModelView("sprint-11.jsp");
+        md.addSession("role", "admin");
+        md.addSession("isConnected", true);
         return md;
     }
 
-/// AllEmp
-    @Url(link = "allEmp.do")
-    @Json
-    public Employes[] getAllEmployes()
+/// Sprint 11
+    @Url(link = "sprint-11.do")
+    @Auth(role = "admin")
+    public ModelView sprint11()
     throws Exception {
-        Employes[] result=new Employes[4];
-        result[0]=new Employes(1, "nom", "prenom");
-        result[1]=new Employes(2, "nom", "prenom");
-        result[2]=new Employes(3, "nom", "prenom");
-        result[3]=new Employes(4, "nom", "prenom");
-        return result;
+        ModelView resut=new ModelView("sprint-11.jsp");
+        return resut;
+    }
+    
+/// Sprint 12
+    @Url(link = "sprint-12.do")
+    public ModelView sprint12()
+    throws Exception {
+        ModelView resut=new ModelView("sprint-12.jsp");
+        return resut;
+    }
+
+/// Sprint 13
+    @Url(link = "sprint-13.do")
+    public ModelView sprint13()
+    throws Exception {
+        ModelView resut=new ModelView("index.html");
+        resut.addItem("nom", this);
+        resut.setJson(true);
+        return resut;
+    }
+
+/// Sprint 14
+    @Url(link = "sprint-12.do")
+    public Employes[] sprint14()
+    throws Exception {
+        Employes[] resut=new Employes[3];
+        resut[0]=new Employes(1, "nom", "prenom");
+        resut[1]=new Employes(2, "nom2", "prenom2");
+        resut[2]=new Employes(3, "nom3", "prenom3");
+        return resut;
+    }
+
+/// Sprint 15-1
+    @Url(link = "sprint-15-1.do")
+    public ModelView sprint15_1()
+    throws Exception {
+        ModelView resut=new ModelView("sprint-15-1.jsp");
+        resut.removeSession("role");
+        return resut;
+    }
+
+/// Sprint 15-2
+    @Url(link = "sprint-15-2.do")
+    public ModelView sprint15_2()
+    throws Exception {
+        ModelView resut=new ModelView("sprint-15-2.jsp");
+        resut.setInvalidateSession(true);
+        return resut;
     }
 }
